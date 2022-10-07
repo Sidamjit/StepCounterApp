71724e6bcf48aa493482e7360300ef756c72f714
dhaliwsida <dhaliwsida@myvuw.ac.nz>
2022-10-07 20:52:34 +1300

    Adding step counter library from ionic native.

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,5 +1,99 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+
+declare let stepcounter: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StepCounterService {
+  constructor() {}
+
+  start(startingOffset) {
+      return new Promise((resolve, reject) => {
+          stepcounter.start(
+              startingOffset,
+              message => {
+                  resolve(message);
+              },
+              () => {
+                  reject();
+              }
+          );
+      });
+  }
+
+  stop() {
+      return new Promise((resolve, reject) => {
+          stepcounter.stop(
+              message => {
+                  resolve(message);
+              },
+              () => {
+                  reject();
+              }
+          );
+      });
+  }
+
+  getTodayStepCount() {
+      return new Promise((resolve, reject) => {
+          stepcounter.getTodayStepCount(
+              message => {
+                  resolve(message);
+              },
+              () => {
+                  reject();
+              }
+          );
+      });
+  }
+
+  getStepCount() {
+      return new Promise((resolve, reject) => {
+          stepcounter.getStepCount(
+              message => {
+                  resolve(message);
+              },
+              () => {
+                  reject();
+              }
+          );
+      });
+  }
+
+  deviceCanCountSteps() {
+      return new Promise((resolve, reject) => {
+          stepcounter.deviceCanCountSteps(
+              message => {
+                  resolve(message);
+              },
+              () => {
+                  reject();
+              }
+          );
+      });
+  }
+
+  getHistory() {
+      return new Promise((resolve, reject) => {
+          stepcounter.getHistory(
+              message => {
+                  resolve(message);
+              },
+              () => {
+                  reject();
+              }
+          );
+      });
+  }
+}
+
+//----- above code from:
+//https://stackoverflow.com/questions/58828456/ionic-4-capacitor-how-to-install-a-cordova-plugin-stepcounter-from-github-wi
+//----- I was struggling to use the ionic native library in this ionic angular project -
+//credits to: Vova Bilyachat from stackoverflow for posting the solution.
 
 @Component({
   selector: 'app-welcome',
@@ -29,7 +123,11 @@ export class WelcomePage implements OnInit {
   status=false;
   startTracker(){
     if(this.status){
-//start tracker here
+      //start tracker here
+      stepcounter.start(0);
+    }
+    else{
+      stepcounter.stop(0);
     }
   }
 }
